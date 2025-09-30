b4eb6922f347c0df1563f6938fd310771ba80778
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-09-30 22:57:37 +0800

    done migrating notificiations and return logs table

--- database/migrations/2025_09_30_125635_create_notifications_table.php
+++ database/migrations/2025_09_30_125635_create_notifications_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->text('message');
+            $table->string('notification_type');
+            $table->dateTime('send_date');
             $table->timestamps();
         });
     }
--- database/migrations/2025_09_30_125705_create_return_logs_table.php
+++ database/migrations/2025_09_30_125705_create_return_logs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\BorrowTransaction;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('return_logs', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(BorrowTransaction::class)->constrained()->onDelete('cascade');
+            $table->date('return_date');
+            $table->string('condition');
+            $table->text('remarks')->nullable();
             $table->timestamps();
         });
     }
