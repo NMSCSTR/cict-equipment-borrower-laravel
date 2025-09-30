c6114330bd67ebe7180d96da260215f39928d5d7
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-10-01 00:34:02 +0800

    Integrating middleware and putting few routes for authentication

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AuthenticateUser::class, 'store'])->name('login.store');
+Route::post('/logout', [AuthenticateUser::class, 'destroy'])->name('logout');
+Route::post('/register', [AuthenticateUser::class, 'register'])->name('register');
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('auth.welcome');
