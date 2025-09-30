5574683ed54ce8628f77b5bb16c3297c2eaf95b0
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-09-30 21:10:33 +0800

    done with user migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('usser_type');
+            $table->enum('name', ['Admin', 'Instructor', 'Student']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('contact_number');
             $table->rememberToken();
             $table->timestamps();
         });
